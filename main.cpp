a64445b258440bfdda1dbae4fb41947a002b4b31
Dante <dalvarezt@unjbg.edu.pe>
2024-07-21 02:26:29 -0500

    Se agrego la función dentro del menú general

--- main.cpp
+++ main.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
-
+#include "cliente.h"
 using namespace std;
-int main(){
+
+int main() {
 	int opcion;
-	do {
 	
-		cout << "1.Cliente" << endl;
+	do {
+		cout << "1. Cliente" << endl;
 		cout << "2. Carta" << endl;
 		cout << "3. Despuensa" << endl;
 		cout << "4. Ventas" << endl;
 		cout << "5. Salir del programa" << endl;
 		cin >> opcion;
+		
 		switch (opcion){
 			case 1:
+				menuCliente();
 				break;
 			case 2:
 				break;
@@ -25,7 +28,9 @@ int main(){
 			default:
 				cout << "Valor ingresado incorrecto, vuelva a ingresar otro valor" << endl;	
 		}
+
 	} while (opcion !=5 );
 		cout << "Programa finalizado" << endl;
+
 	return 0;
 }
