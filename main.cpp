c1b3b942dd51d1816e97bee3e70b6334d5a2f1ec
Alexander <achicallag@unjbg.edu.pe>
2024-07-21 00:13:17 -0500

    Proyecto iniciado
    Se añadieron los archivos bases para el desarrollo del proyecto final

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+	int opcion;
+	do {
+	
+		cout << "1.Cliente" << endl;
+		cout << "2. Carta" << endl;
+		cout << "3. Despuensa" << endl;
+		cout << "4. Ventas" << endl;
+		cout << "5. Salir del programa" << endl;
+		cin >> opcion;
+		switch (opcion){
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			default:
+				cout << "Valor ingresado incorrecto, vuelva a ingresar otro valor" << endl;	
+		}
+	} while (opcion !=5 );
+		cout << "Programa finalizado" << endl;
+	return 0;
+}
